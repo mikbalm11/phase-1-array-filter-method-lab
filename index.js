4f415229a87bfb54fa49542203bb02e2bf1e329e
Mustafa Mehmedoglu <mikbalm@gmail.com>
2024-04-08 14:48:47 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching (array, str) {
+    return array.filter(name => name.toLowerCase() === str.toLowerCase());
+}
+
+function fuzzyMatch (array, str) {
+    return array.filter(name => name.slice(0, str.length) === str);
+}
+
+function matchName (array, str) {
+    return array.filter( function (item) {
+        if (item.name === str) {
+            return item;
+        }
+    });
+}
